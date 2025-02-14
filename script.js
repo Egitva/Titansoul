0e5f60610f2ad43bde6520d1d991da57c4cbfd07
Egitva <9026479948e@gmail.com>
2025-02-14 12:50:56 +0500

    cjplfk

---
+++ script.js
@@ -0,0 +1,17 @@
+// Массив с фразами
+const phrases = [
+    "Всё получится!",
+    "Никогда не сдавайся!",
+    "Успех ближе, чем кажется!",
+    "Каждый день — новый шанс!",
+    "Действуй, и всё получится!"
+];
+
+document.getElementById('generator').onclick = getRandomPhrase;
+
+function getRandomPhrase() {
+    var pass =''
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    pass = phrases[randomIndex];
+    document.getElementById('out').innerHTML='<p>'+pass+'</p>';
+}
