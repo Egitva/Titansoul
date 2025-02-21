9411765acb485f2f4b5d9f0af3e439228ef49d0d
Egitva <9026479948e@gmail.com>
2025-02-21 22:37:44 +0500

    факты

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 // Массив с фразами
-const phrases = [
-    "Всё получится!",
-    "Никогда не сдавайся!",
-    "Успех ближе, чем кажется!",
-    "Каждый день — новый шанс!",
-    "Действуй, и всё получится!"
-];
-
+img = document.getElementById("gi");
+const Nelo = [
+    "Нело изначально должен был быть расистом",
+    "Огонь Нело, как и других огненных людей, выделяет кислород и не обжигает"
+]
+const Kiken = [
+    "Изначалько Кейкен должен был быть психически нестабильным",
+    "Кейкен пахнет ванилью"
+]
+const Rossa = [
+    "Шляпа Россы изначально принадлежала Лойду",
+    "В 16 лет, Росса могла бы стать чемпионом по боксу в среднем весе, но предпочла провести время с Кейкеном и Лойдом"
+]
+const Holden = [
+    "Холден не сделал ничего плохого",
+    "Холден имеет докторскую степень по биологии",
+    "У Холдена в общем 3 дочери",
+    "На момент 3 главы, Холдену 102 года",
+    "Холден не стареет",
+    "Рост Холдена - 282см",
+]
 document.getElementById('generator').onclick = getRandomPhrase;
 
 function getRandomPhrase() {
     var pass =''
-    const randomIndex = Math.floor(Math.random() * phrases.length);
-    pass = phrases[randomIndex];
-    document.getElementById('out').innerHTML='<p>'+pass+'</p>';
-}
-
-document.getElementById('menu').onclick = Visibility;
-
-function Visibility() {
-    document.getElementById('sidenav').classList.add('hidden');
+    const randomChar = Math.floor(Math.random() * 4);
+    if (randomChar == 0) {
+        const randomIndex = Math.floor(Math.random() * Nelo.length);
+        pass = Nelo[randomIndex];
+        img.src = "images/Chars/Nelo.jpg";
+        document.getElementById('out').innerHTML='<p>'+pass+'</p>';
+    }
+    else if (randomChar == 1) {
+        const randomIndex = Math.floor(Math.random() * Kiken.length);
+        pass = Kiken[randomIndex];
+        img.src = "images/Chars/Kiken.jpg";
+        document.getElementById('out').innerHTML='<p>'+pass+'</p>';
+    }
+    else if (randomChar == 2) {
+        const randomIndex = Math.floor(Math.random() * Rossa.length);
+        pass = Rossa[randomIndex];
+        img.src = "images/Chars/Rossa.jpg";
+        document.getElementById('out').innerHTML='<p>'+pass+'</p>';
+    }
+    else if (randomChar == 3) {
+        const randomIndex = Math.floor(Math.random() * Holden.length);
+        pass = Holden[randomIndex];
+        img.src = "images/Chars/Holden.jpg";
+        document.getElementById('out').innerHTML='<p>'+pass+'</p>';
+    }
 }
