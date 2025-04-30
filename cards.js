b533e519bb8f38a6ee63962039d5c6f83055db44
Egitva <9026479948e@gmail.com>
2025-04-30 16:45:47 +0500

    rforjodrk

---
+++ cards.js
@@ -0,0 +1,8 @@
+var draw = document.getElementById("draw");
+var counter = 1;
+card_list = [
+    "negr"
+]
+draw.onclick = function() {
+    document.getElementById("cards").innerHTML+=`<img src='mangaPages/CH1/TS0.jpg' style="width: 50; height: 50;">`;
+}
