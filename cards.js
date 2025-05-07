06604dd61763ff0418d7a344f6463514d165c396
Egitva <9026479948e@gmail.com>
2025-05-07 11:44:57 +0500

    SCDKMF

--- cards.js
+++ cards.js
@@ -4,5 +4,5 @@ card_list = [
     "negr"
 ]
 draw.onclick = function() {
-    document.getElementById("cards").innerHTML+=`<img src='mangaPages/CH1/TS0.jpg' style="width: 50; height: 50;">`;
+    document.getElementById("cards").innerHTML+=`<img src='mangaPages/CH1/TS0.jpg' style="width: 50; height: 50; text-align: center; margin: 5px;">`;
 }
