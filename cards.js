4d5441b6a5600b5ac63e0d207aefe9dffb881cb1
Egitva <9026479948e@gmail.com>
2025-05-16 15:16:22 +0500

    поклп

--- cards.js
+++ cards.js
@@ -1,6 +1,5 @@
 var draw = document.getElementById("draw");
 var counter = 1;
-var carda = document.getElementById("carda");
 var canvase = document.getElementById("canvase");
 deck = [
     "F1",
@@ -29,11 +28,13 @@ draw.onclick = function() {
         randomChar = Math.floor(Math.random() * deck.length);
         texto = `images/Cards/${deck[randomChar]}.png`;
     };
-    document.getElementById("cards").innerHTML += `<img id="carda" class="carda" src='${texto}'>`;
+    document.getElementById("cards").innerHTML += `<img class="carda" src='${texto}'>`;
     hand.push(deck[randomChar]);
     deck.splice(randomChar, 1);
     }
 }
-carda.onclick = function() {
-    canvase.innerHTML += `<img id="carda" class="carda" src='images/Cards/$F10.png'>`;
-}
+document.getElementById("cards").addEventListener('click', (event) => {
+    if (event.target && event.target.classList.contains('carda')) {
+        canvase.innerHTML += `<img class="carda" src='images/Cards/F10.png'>`;
+    }
+});
